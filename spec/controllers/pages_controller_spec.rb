c3d4920e63603ece873736d2ef6b32e2a6c661ae
Unknown <marderer@bk.ru>
2018-02-21 15:33:27 +0300

    Add first test

---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe PagesController, type: :controller do
+  describe "GET home" do
+    it "has a 200 status code" do
+      get :show, params: { 'page' => 'home' }
+      expect(response.status).to eq(200)
+    end
+
+    it "has a 404 status code" do
+      get :show, params: { 'page' => 'asdasd' }
+      expect(response.status).to eq(404)
+    end
+  end
+end
