01eb50d2ec9ec1c41ecd626b1ab80eee73529944
Unknown <marderer@bk.ru>
2018-02-24 00:43:49 +0300

    Robocop refactoring

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe PagesController, type: :controller do
-  describe "GET home" do
-    it "has a 200 status code" do
+  describe 'GET home' do
+    it 'has a 200 status code' do
       get :show, params: { 'page' => 'home' }
       expect(response.status).to eq(200)
     end
 
-    it "has a 404 status code" do
+    it 'has a 404 status code' do
       get :show, params: { 'page' => 'asdasd' }
       expect(response.status).to eq(404)
     end
