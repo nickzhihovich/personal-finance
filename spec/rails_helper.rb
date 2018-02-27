644e867d92e0eb610134d93ae4899691318ace63
Unknown <marderer@bk.ru>
2018-02-28 00:41:20 +0300

    Add ten transactions to home page and templates for transactions

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -14,4 +14,6 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
 
   config.filter_rails_from_backtrace!
+
+  config.include Devise::Test::ControllerHelpers, type: :controller
 end
