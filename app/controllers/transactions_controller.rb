36047982e42bc17730136dedbf12c98e3018c8d3
Unknown <marderer@bk.ru>
2018-02-28 14:41:46 +0300

    Add CRUD actions to transactions

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,4 +1,5 @@
 class TransactionsController < ApplicationController
+  before_action :find_transaction, only: %i[update destroy edit]
   def new
     @transaction = current_user.transactions.new
   end
@@ -12,7 +13,21 @@ class TransactionsController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @transaction.update(transaction_params)
+      redirect_to root_path
+    else
+      render 'edit'
+    end
+  end
+
   private
+
+  def find_transaction
+    @transaction = Transaction.find(params[:id])
+  end
 
   def transaction_params
     params.require(:transaction).permit(
