77878a8122205b6de52fbfc4c7510484d87bdaf3
Nick <nickzhihovich@gmail.com>
2018-03-02 15:15:20 +0300

    Merge pull request #14 from marderer/username
    Add username for users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,9 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[email password password_confirmation remember_me
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[username email password password_confirmation remember_me
                                                          avatar avatar_cache remove_avatar])
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[email password password_confirmation current_password
-                                                                avatar avatar_cache remove_avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[username email password password_confirmation
+                                                                current_password avatar avatar_cache remove_avatar])
   end
 end
