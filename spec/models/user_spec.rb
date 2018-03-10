c12afae3e8fc258ec57393104a192c646f373c8f
Unknown <marderer@bk.ru>
2018-03-10 22:57:05 +0300

    Add balance to header

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,4 +14,13 @@ RSpec.describe User, type: :model do
       expect(user).to_not be_valid
     end
   end
+
+  describe 'balance' do
+    it 'calculate' do
+      user = create(:user)
+      transaction1 = create(:transaction, user_id: user.id)
+      transaction2 = create(:transaction, user_id: user.id)
+      expect(user.balance).to eq(transaction1.amount.to_i + transaction2.amount.to_i)
+    end
+  end
 end
