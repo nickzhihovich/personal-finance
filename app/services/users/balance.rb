3c5e2407647ca33666afc44e8517b3a2e8bac09a
Unknown <marderer@bk.ru>
2018-04-10 00:06:11 +0300

    Change balance after created expenses transactions

--- app/services/users/balance.rb
+++ app/services/users/balance.rb
@@ -6,8 +6,9 @@ class Users::Balance < Struct.new(:user)
   private
 
   def transactions_amount
-    user_balance_transactions.sum(&:amount)
+    user_balance_transactions.sum(&:amount) - user_expense_transactions.sum(&:amount)
   end
 
   delegate :balance_transactions, to: :user, prefix: true
+  delegate :expense_transactions, to: :user, prefix: true
 end
