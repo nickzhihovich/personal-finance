91ede75ef795715331c03db19e4c60dc3ea7a2ec
Unknown <marderer@bk.ru>
2018-04-09 22:49:14 +0300

    Add scope for last ten transactions on home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   def home
     return unless user_signed_in?
     @form = BalanceTransactionForm.new(Transaction.new, balance_transactions: BalanceTransaction.new)
-    @transactions = current_user.transactions.includes(:transactinable).limit(10).order('id desc')
+    @last_transactions = current_user.transactions_last_ten
     @categories = current_user.categories.main_category
     @date = Charts::Home::CategoriesDate.new(current_user.categories_with_amount).data
   end
