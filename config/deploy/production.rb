4f2b915eeae666abbb76b0994fe64e8f0255aa53
Nick <nickzhihovich@gmail.com>
2018-06-03 23:17:51 +0300

    mend

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 server '13.59.53.131', user: 'ubuntu', roles: %w[web app db]
 set :ssh_options, forward_agent: true
 
-before 'deploy:assets:precompile', 'deploy:yarn_install'
+before 'deploy:assets:precompile', 'webpacker:compile'
 
 
